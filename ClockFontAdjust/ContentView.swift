fc5818c8b132a363555fff66e88fb9085c3e6688
fanzhiyong10 <13700318430@163.com>
2022-12-20 17:47:21 +0800

    add: current time

--- ClockFontAdjust/ContentView.swift
+++ ClockFontAdjust/ContentView.swift
@@ -9,25 +9,37 @@ import SwiftUI
 
 ///Text font size will change if device orientation change.
 struct ContentView: View {
-    /// device orientation
+    //MARK: device orientation
     @State private var isPortrait:Bool = (UIScreen.main.bounds.height > UIScreen.main.bounds.width)
     
-    /// Notification: orientation change
+    //MARK: Notification: orientation change
     private let orientationPublisher = NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)
+    
+    //MARK: timer
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    //MARK: current time
+    @State var str_current_time = "\(Date().formatted(date: .omitted, time: .standard))"
 
     var body: some View {
         Group{
             if self.isPortrait {
                 HStack(spacing: 0){
-                    Text("\(Date().formatted(date: .omitted, time: .standard))")
+                    Text(str_current_time)
                         .font(.system(size: UIScreen.main.bounds.width / 6, weight: .bold))
                         .padding()
+                        .onReceive(timer) { time in
+                            self.str_current_time = "\(Date().formatted(date: .omitted, time: .standard))"
+                        }
                 }
             } else {
                 VStack(spacing: 0){
-                    Text("\(Date().formatted(date: .omitted, time: .standard))")
+                    Text(str_current_time)
                         .font(.system(size: UIScreen.main.bounds.width / 6, weight: .bold))
                         .padding()
+                        .onReceive(timer) { time in
+                            self.str_current_time = "\(Date().formatted(date: .omitted, time: .standard))"
+                        }
                 }
             }
         }
