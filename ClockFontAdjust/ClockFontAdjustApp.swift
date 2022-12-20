04419b6d7fd51169fb68bf62be86120b2cc273e7
fanzhiyong10 <13700318430@163.com>
2022-12-20 14:39:05 +0800

    Initial Commit

---
+++ ClockFontAdjust/ClockFontAdjustApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockFontAdjustApp.swift
+//  ClockFontAdjust
+//
+//  Created by 范志勇 on 2022/12/20.
+//
+
+import SwiftUI
+
+@main
+struct ClockFontAdjustApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
