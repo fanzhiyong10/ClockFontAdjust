055343ce24eb414486b615b8a3a9d47a59009082
fanzhiyong10 <13700318430@163.com>
2022-12-20 15:39:07 +0800

    Text font size will change if device orientation change. 文本大小的变化随着设备方向的变化而变化.

--- ClockFontAdjust/ContentView.swift
+++ ClockFontAdjust/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    /// device orientation
+    @State private var isPortrait:Bool = (UIScreen.main.bounds.height > UIScreen.main.bounds.width)
+    
+    /// orientation change
+    private let orientationPublisher = NotificationCenter.default.publisher(for: UIDevice.orientationDidChangeNotification)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Group{
+            if self.isPortrait {
+                HStack(spacing: 0){
+                    Text("\(Date().formatted(date: .omitted, time: .standard))")
+                        .font(.system(size: UIScreen.main.bounds.width / 6, weight: .bold))
+                        .padding()
+                }
+            } else {
+                VStack(spacing: 0){
+                    Text("\(Date().formatted(date: .omitted, time: .standard))")
+                        .font(.system(size: UIScreen.main.bounds.width / 6, weight: .bold))
+                        .padding()
+                }
+            }
+        }
+        .onReceive(orientationPublisher) { _ in
+            switch (UIDevice.current.orientation){
+            case .portrait:
+                self.isPortrait = true
+            case .landscapeLeft, .landscapeRight:
+                self.isPortrait = false
+            default:
+                break
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewInterfaceOrientation(.portrait)
     }
 }
